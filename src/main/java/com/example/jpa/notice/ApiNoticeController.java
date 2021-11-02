c9bf37f64477e810246cba350def91f1c3bb5c00
parkyuseok <znql16@gmail.com>
2021-11-02 12:42:32 +0900

    feat: builder pattern 사용하기

--- src/main/java/com/example/jpa/notice/ApiNoticeController.java
+++ src/main/java/com/example/jpa/notice/ApiNoticeController.java
@@ -1,6 +1,8 @@
 package com.example.jpa.notice;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,6 +35,7 @@ public class ApiNoticeController {
 	}
 	*/
 	
+	/*
 	@GetMapping("/api/notice")
 	public NoticeModel notice() {
 		
@@ -45,5 +48,30 @@ public class ApiNoticeController {
 		notice.setRegDate(regDate);
 		
 		return notice;
+	}
+	*/
+	
+	@GetMapping("/api/notice")
+	public List<NoticeModel> notice() {
+		
+		List<NoticeModel> noticeList = new ArrayList<>();
+		
+		NoticeModel notice1 = new NoticeModel();
+		notice1.setId(1);
+		notice1.setTitle("공지사항입니다.");
+		notice1.setContents("공지사항 내용입니다.");
+		notice1.setRegDate(LocalDateTime.of(2021, 2, 8, 0, 0));
+		noticeList.add(notice1);
+		
+		// builder pattern 사용 - List형태의 데이터에 사용하면 편리하다.
+		NoticeModel notice2 = NoticeModel.builder()
+				.id(2)
+				.title("두번째 공지사항입니다.")
+				.contents("두번째 공지사항 내용입니다.")
+				.regDate(LocalDateTime.of(2021, 2, 8, 0, 0))
+				.build();
+		noticeList.add(notice2);
+		
+		return noticeList;
 	}
 }
--- src/main/java/com/example/jpa/notice/model/NoticeModel.java
+++ src/main/java/com/example/jpa/notice/model/NoticeModel.java
@@ -2,7 +2,10 @@ package com.example.jpa.notice.model;
 
 import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @packageName : com.example.jpa.notice
@@ -16,11 +19,14 @@ import lombok.Data;
  * DATE           AUTHOR       NOTE
  * -----------------------------------------------------------
  * 2021.11.02     박유석               최초 생성
+ * 2021.11.02     박유석               가장 많이 사용하는 lombok 어노테이션 추가
  * </pre>
  */
 
-// 2. @Data을 이용해서 getter setter를 편하게 사용할 수 있다.
-@Data
+@AllArgsConstructor //전체 내용에 대한 생성자
+@NoArgsConstructor  //생성자 부분에 대한 이슈가 발생할 수 있으므로 default 생성자를 만들어준다.
+@Builder            //데이터를 입력할 때 Builder pattern을 이용, static한 클래스를 이용해서 편하게 사용할 수 있는 형태
+@Data               //getter setter를 편하게 사용할 수 있다.
 public class NoticeModel {
 
 	// ID, 제목, 내용, 등록일(작성일)
@@ -29,5 +35,5 @@ public class NoticeModel {
 	private String contents;
 	private LocalDateTime regDate; // 요즘 여러가지 이유로 Date type은 사용하지 않는다.
 	
-	// 1. private이므로 외부에서 사용하기 위해서는 getter, setter가 필요하다.
+	// private이므로 외부에서 사용하기 위해서는 getter, setter가 필요하다.
 }
