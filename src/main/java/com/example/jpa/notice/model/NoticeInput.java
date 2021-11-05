fad834493e6eabfafac69032b1b3bdc286f39997
parkyuseok <znql16@gmail.com>
2021-11-05 12:42:24 +0900

    글을 작성할 때 제목과 내용을 받아서 저장하는 API 만들기 - 글자 수 제한 조건(@Size)
    [조건]
    - 입력값은 입력DTO를 통해서 입력받음
    - 제목과 내용은 필수 입력 조건임(입력되지 않은 경우 400 리턴)
    - 제목의 경우 10자이상 100자 이하로 입력
    - 내용의 경우 50자이상 1000자 이하로 입력
    - 예외 발생시 각각의 에러를 취합하여 콜렉션형태로 리턴

--- src/main/java/com/example/jpa/notice/model/NoticeInput.java
+++ src/main/java/com/example/jpa/notice/model/NoticeInput.java
@@ -1,6 +1,7 @@
 package com.example.jpa.notice.model;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -29,9 +30,11 @@ import lombok.NoArgsConstructor;
 @Data
 public class NoticeInput {
 
+	@Size(min = 10, max = 100, message = "제목은 10-100자 사이의 값입니다.")
 	@NotBlank(message = "제목은 필수 항목입니다.")
 	private String title;
 	
+	@Size(min = 50, max = 1000, message = "제목은 50-1000자 사이의 값입니다.")
 	@NotBlank(message = "내용은 필수 항목입니다.")
 	private String contents;
 	
