0c1f48cd40998f4b8abee4f09a997a10028557e1
parkyuseok <znql16@gmail.com>
2021-11-16 17:20:39 +0900

    style: 오타수정

--- src/main/java/com/example/jpa/notice/model/NoticeInput.java
+++ src/main/java/com/example/jpa/notice/model/NoticeInput.java
@@ -34,7 +34,7 @@ public class NoticeInput {
 	@NotBlank(message = "제목은 필수 항목입니다.")
 	private String title;
 	
-	@Size(min = 50, max = 1000, message = "제목은 50-1000자 사이의 값입니다.")
+	@Size(min = 50, max = 1000, message = "내용은 50-1000자 사이의 값입니다.")
 	@NotBlank(message = "내용은 필수 항목입니다.")
 	private String contents;
 	
