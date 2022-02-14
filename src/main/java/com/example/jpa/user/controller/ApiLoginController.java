09f1482f302d3730696b1648ce82ef158400178d
parkyuseok <znql16@gmail.com>
2022-02-14 17:27:10 +0900

    feat: 로그인시 에러가 발생하는 경우 로그에 기록하는 기능을 작성해 보세요. - application.yml에 설정을 추가하여 log를 파일로 저장함

--- src/main/java/com/example/jpa/user/controller/ApiLoginController.java
+++ src/main/java/com/example/jpa/user/controller/ApiLoginController.java
@@ -18,6 +18,7 @@ import com.example.jpa.user.service.UserService;
 import com.example.jpa.util.JWTUtils;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @packageName : com.example.jpa.user.controller
@@ -34,6 +35,7 @@ import lombok.RequiredArgsConstructor;
  * </pre>
  */
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class ApiLoginController {
@@ -41,8 +43,8 @@ public class ApiLoginController {
 	private final UserService userService;
 	
 	/**
-	 * 회원 로그인 히스토리 기능을 구현하는 API를 작성해 보세요.
-	 */
+	 * Q83. 회원 로그인 히스토리 기능을 구현하는 API를 작성해 보세요.
+	
 	@PostMapping("/api/login")
 	public ResponseEntity<?> login(@RequestBody @Valid UserLogin userLogin, Errors errors) {
 		
@@ -59,6 +61,34 @@ public class ApiLoginController {
 		UserLoginToken userLoginToken = JWTUtils.createToken(user);
 		
 		if (userLoginToken == null) {
+			return ResponseResult.fail("JWT 생성에 실패하였습니다.");
+		}
+		return ResponseResult.success(userLoginToken);
+	} */
+	
+	/**
+	 * Q84. 로그인시 에러가 발생하는 경우 로그에 기록하는 기능을 작성해 보세요.
+	 */
+	@PostMapping("/api/login")
+	public ResponseEntity<?> login(@RequestBody @Valid UserLogin userLogin, Errors errors) {
+		
+		log.info("로그인 함수 !!!!");
+		
+		if (errors.hasErrors()) {
+			return ResponseResult.fail("입력값이 정확하지 않습니다.", ResponseError.of(errors.getAllErrors()));
+		}
+		
+		User user = null;
+		try {
+			user = userService.login(userLogin);
+		} catch (BizException e) {
+			log.info("로그인 에러: " + e.getMessage());
+			return ResponseResult.fail(e.getMessage());
+		}
+		UserLoginToken userLoginToken = JWTUtils.createToken(user);
+		
+		if (userLoginToken == null) {
+			log.info("JWT 생성에 실패하였습니다.");
 			return ResponseResult.fail("JWT 생성에 실패하였습니다.");
 		}
 		return ResponseResult.success(userLoginToken);
